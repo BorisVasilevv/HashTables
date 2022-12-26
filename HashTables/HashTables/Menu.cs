e1356f0eabe23ef7f7fd8acfb0c63e4c56e7998c
Boris <BacR009@yandex.ru>
2022-12-26 20:29:35 +0500

    Add Menu

---
+++ HashTables/HashTables/Menu.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HashTables
+{
+    class Menu
+    {
+        string _question;
+        List<string> _answers;
+
+        public Menu(string question, List<string> answers)
+        {
+            _answers = answers;
+            
+            _question = question;
+        }
+
+        public Menu(string question, params string[] items)
+        {
+            _answers = new List<string>();
+            foreach (string item in items)
+            {
+                _answers.Add(item);
+                
+            }
+            _question=question;
+        }
+
+        int NumberOfSelectedElem = 0;
+
+        public int GetIndexOfAnswer()
+        {
+            int answer;
+            DrawMenu();
+            answer = NumberOfSelectedElem;
+            NumberOfSelectedElem = 0;
+            ClearConsole();
+            return answer;
+        }
+
+        public void DrawMenu()
+        {
+            bool isUserChoose = false;
+            
+            Console.WriteLine(_question);
+            Console.CursorVisible = false;
+            while(!isUserChoose)
+            {
+                ClearConsole();
+                for(int i=0;i<_answers.Count;i++)
+                {
+                    if(i==NumberOfSelectedElem) Console.BackgroundColor=ConsoleColor.Cyan;
+                    Console.WriteLine(_answers[i]);
+                    Console.BackgroundColor=ConsoleColor.Black;                   
+                }
+
+
+                ConsoleKeyInfo key= Console.ReadKey(false);
+
+                switch (key.Key)
+                {
+                    case ConsoleKey.UpArrow:
+                        ChangeSelection(false);
+                        break;
+                    case ConsoleKey.DownArrow:
+                        ChangeSelection(true);
+                        break;
+                    case ConsoleKey.Enter:
+                        isUserChoose = true;
+                        
+                        break;
+
+                }
+            }
+        }
+
+
+        void ChangeSelection(bool selectedDecrease)
+        {
+            if(selectedDecrease)
+            {
+                NumberOfSelectedElem++;
+                if (NumberOfSelectedElem >= _answers.Count) NumberOfSelectedElem = 0;
+            }
+            else
+            {
+                NumberOfSelectedElem--;
+                if(NumberOfSelectedElem<0)NumberOfSelectedElem=_answers.Count-1;
+            }
+        }
+
+
+        void ClearConsole()
+        {
+            for(int i=0;i<Console.WindowHeight;i++)
+            {
+                Console.SetCursorPosition(0, i);
+                Console.WriteLine(new string(' ', Console.WindowWidth));
+            }
+            Console.SetCursorPosition(0, 0);
+        }
+    }
+}
---
+++ HashTables/HashTables/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace HashTables
+{
+    class Program
+    {
+        const int ChainsMethodIndex = 0;
+        const int StraightAdresMethodIndex = 1;
+        const int ExitIndex = 2;
+
+
+        public static void Main(string[] args)
+        {
+            Menu chooseTaskMenu = new Menu("Выберите задание:",
+                "Метод разрешения коллизий с помощью цепочек", "Метод открытой адресации для разрешения коллизий", "Выйти");
+            int indexOfAnswer = chooseTaskMenu.GetIndexOfAnswer();
+            if (indexOfAnswer == ExitIndex) System.Environment.Exit(0);
+            else if (indexOfAnswer == ChainsMethodIndex) Console.WriteLine("Ch");
+            else if (indexOfAnswer == StraightAdresMethodIndex) Console.WriteLine("St");
+
+            Console.ReadKey();
+
+
+
+
+            Menu workWithTableMenu = new Menu("Выберите, что вы хотите сделать:",
+                "Добавить элемент в таблицу", "Найти элемент", "Удалить элемент", "Показать таблицу" ,"Выйти");
+        }
+    }
+}
